952ffdd343e13e7f8b9aa1c82bdd20ff727b202b
Craig <craigsheehy123@gmail.com>
2023-05-14 00:00:48 +0100

    - Painfully fixed the whole thing

--- OOPCoffeeMachine/main.py
+++ OOPCoffeeMachine/main.py
@@ -11,62 +11,20 @@ menu = Menu()  # Create a Menu Object
 money = MoneyMachine()  # Creating a MoneyMachine Object
 
 switch = True
-# Making a list of all the items in the menu
+
 while switch:
-    user_input = input("What would you like? (espresso/latte/cappuccino): ").lower()
-    items = menu.get_items()
+    options = menu.get_items()
+    selection = input(f'What you would like ({options}): ')
 
-    if user_input in items:
-        drinks = menu.find_drink(user_input)  # Finding the ingredients for the selected item
-        cost_forSelected = drinks.cost  # Finding the cost for the selected item
+    if selection == 'report':
+        coffee.report()
+        money.report()
 
-        # Printing what the user selected
-        print(f"You selected {user_input}.")
-
-        # Check if we even have the ingredients
-        if coffee.is_resource_sufficient(drinks):
-            print('We have enough to make this')
-
-            # Printing the ingredients needed
-            print("Ingredients needed: ", drinks.ingredients)
-
-            # TODO: Make a check for if we go negative and if stop prompt user saying not enough resources
-            # TODO: Subtract the ingredients from the resources
-
-            coffee.resources['water'] -= drinks.ingredients['water']
-            coffee.resources['milk'] -= drinks.ingredients['milk']
-            coffee.resources['coffee'] -= drinks.ingredients['coffee']
-
-            # Printing the cost for selected
-            print("Cost €: ", cost_forSelected)
-
-            # Make payment
-            money = money_machine.MoneyMachine()
-            money_received = money.process_coins()
-            print("€", money_received)
-
-            # TODO:  Add the price of the drink to the bank adn return change
-
-            # Check transaction successful
-            if money_received >= cost_forSelected:
-                # Dispensing Process
-                coffee.make_coffee(drinks)
-                print('Successful transaction')
-                switch = False
-
-            else:
-                print('Transaction failed')
-                switch = False
-
-    if user_input == 'report'.lower():
-        coffee.report()  # Creating a report of the resources
-        money.report()  # Creating a report of the money
-        # switch = False
-
-    elif user_input == 'off'.lower():
+    elif selection == 'off':
         switch = False
 
     else:
-        switch = True
-        # offer_again = menu.find_drink(user_input)
-        # print(offer_again) # FIX ME
+        drink = menu.find_drink(selection)
+        if coffee.is_resource_sufficient(drink) and money.make_payment(drink.cost):
+            coffee.make_coffee(drink)
+
