cf841dfd1c163405cba68796926565adcb2e4782
Craig Sheehy <craigsheehy123@gmail.com>
2023-05-16 20:20:35 +0100

    - Started Daminen Hirst Painting

---
+++ HirstPainting/main.py
@@ -0,0 +1,47 @@
+import colorgram
+from turtle import Turtle, Screen
+import random
+
+# Turtle object
+# squiddy = Turtle()
+
+# == Attributes ==
+# squiddy.shape('turtle')
+# squiddy.color('red')
+# squiddy.speed('fastest')
+
+colour_search = 5
+
+colours = colorgram.extract('hirst.jpg', colour_search)
+# print(colours)
+
+# Loop that finds displays most common colours in rgb
+# for _ in range(colour_search):
+#     rgb_colours = []
+#     r = colours[_].rgb.r
+#     g = colours[_].rgb.g
+#     b = colours[_].rgb.b
+#
+#     colour = (r, g, b)
+#     rgb_colours.append(colour)
+#     print(rgb_colours)
+
+
+colours = [
+(246, 245, 243)
+(233, 239, 246)
+(246, 239, 242)
+(240, 246, 243)
+(132, 166, 205)
+]
+
+
+
+
+# == Functions ==
+def colour_from_colorgram():
+    pass
+
+
+def million_euro_painting():
+    pass
--- TurtlePainting/main.py
+++ TurtlePainting/main.py
@@ -1,37 +1,60 @@
+import turtle
 from turtle import Turtle, Screen
+import random
 
 # Creating tutle boi
 squidward = Turtle()
+turtle.colormode(255)
 squidward.shape('turtle')
 squidward.color('green')
-
-# Drawing a Triangle
-def triangle():
-    for _ in range(3):
-        squidward.right(120)
-        squidward.forward(100)
-
-def square():
-    for _ in range(4):
-        squidward.right(90)
-        squidward.forward(100)
-
-def pentagon():
-    for _ in range(6):
-        squidward.right(60)
-        squidward.forward(100)
+squidward.speed('fastest')
 
 
-triangle()
-square()
-pentagon()
-def hexagon():
+def random_colour():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
+
+
+def draw_spirograph(degree):
+    for _ in range(degree):
+        squidward.color(random_colour())
+        squidward.circle(100)
+        squidward.left(360 / degree)
+
+
+# draw_spirograph(20)
+
+def million_euro_painting():
     pass
-    # for _ in range
+
+# Draw shape
+def draw_shape(num_of_sides):
+    angle = 360 / num_of_sides
+    for _ in range(num_of_sides):
+        squidward.forward(100)
+        squidward.right(angle)
 
 
+# for shape_side_n in range(3, 11):
+#     draw_shape(shape_side_n)
 
 
+def random_walk():
+    angles = [0, 90, 180, 270]
+    squidward.pensize(10)
+    squidward.speed('fastest')
+
+    for _ in range(500):
+        squidward.hideturtle()
+        squidward.pencolor(random_colour())
+        # squidward.color(random.choice(['red', 'green', 'blue', 'yellow', 'purple', 'pink', 'black']))
+        squidward.forward(25)
+        squidward.setheading(random.choice(angles))
+
+
+# random_walk()
 
 # Drawing a dotted line
 # def draw_dotted(space, x):
@@ -52,19 +75,6 @@ def hexagon():
 # for _ in range(4):
 #     squidward.forward(100)
 #     squidward.right(90)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 # Creating screen module
