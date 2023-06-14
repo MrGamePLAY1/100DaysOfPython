1229c0bdb9a896b2060700eb119a151b3bbf6789
Craig Sheehy <craigsheehy123@gmail.com>
2023-06-14 12:40:09 +0100

    - Cleaned code

--- Automatedmailer/main.py
+++ Automatedmailer/main.py
@@ -6,15 +6,13 @@ letter_contents = letter.read()
 
 # for loop getting the names in invited_names.txt
 for name in names:
-    name = name.strip()# gets the name stripped of the new line
+    name = name.strip()  # gets the name stripped of the new line
 
     new_letter = letter_contents.replace("[name]", name)
     with open(f"Output/ReadyToSend/{name}.txt", mode="w") as completed_letter:
         completed_letter.write(new_letter)
         completed_letter.close()
         print(f"Created {name}.txt")
-
-
 
 # Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
 # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
