330facee8dd43f6afcb01ed402940d2cad0902ff
Craig Sheehy <craigsheehy123@gmail.com>
2023-06-15 10:37:06 +0100

    - Introduction to Pandas

--- ReadingCSV/main.py
+++ ReadingCSV/main.py
@@ -6,9 +6,12 @@ import pandas as pd
 #     data = path.readlines()
 #     print(type(data))
 
-with open ("weather_data.csv") as path:
-    data = csv.reader(path)
-    temp = []
-    for row in data:
-        if row[1] != 'temp':
-            temp.append(int(row[1]))
+# with open ("weather_data.csv") as path:
+#     data = csv.reader(path)
+#     temp = []
+#     for row in data:
+#         if row[1] != 'temp':
+#             temp.append(int(row[1]))
+
+data = pd.read_csv("weather_data.csv")
+print(data['temp'])
