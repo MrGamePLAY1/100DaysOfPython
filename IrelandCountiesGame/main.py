95c3a924db4aa3cf3cc33e29329f67435c8bfb0f
Craig Sheehy <craigsheehy123@gmail.com>
2023-06-19 15:05:40 +0100

    - Implemented basic functionality to check counties

--- IrelandCountiesGame/main.py
+++ IrelandCountiesGame/main.py
@@ -4,14 +4,29 @@ from functions import *
 screen = turtle.Screen()
 screen.setup(width=600, height=600)
 screen.title('Ireland Map')
-
 screen.addshape('ireland.gif')
-
 turtle.shape('ireland.gif')
 
+# Reading the CSV
+read = open('Ireland_Counties.csv', 'r')
+
+# Counties
+counties = []
+for line in read:
+    counties.append(line.lower().split(',')[1])
+
+answer = screen.textinput(title='Ireland Map', prompt='Name a county!')
+
+while answer.lower() != 'done':
+    if answer in counties:
+        print(answer)
+    else:
+        print('incorrect')
+    answer = screen.textinput(title='Ireland Map', prompt='Name a county!')
+
 # Getting co-ordinates on click
-turtle.onscreenclick(get_position_on_click)
-turtle.mainloop()
+# turtle.onscreenclick(get_position_on_click)
+# turtle.mainloop()
 
 
 # screen.exitonclick()
