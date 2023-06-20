8f93b34fcc0ae8bf30017b2a8eb5ca2626174544
Craig Sheehy <craigsheehy123@gmail.com>
2023-06-20 10:19:30 +0100

    - Added end game - Added max score

--- IrelandCountiesGame/main.py
+++ IrelandCountiesGame/main.py
@@ -2,7 +2,6 @@ import turtle
 from functions import *
 import pandas as pd
 
-
 screen = turtle.Screen()
 screen.setup(width=600, height=600)
 screen.title('Ireland Map')
@@ -14,15 +13,21 @@ read = pd.read_csv('ireland.csv')
 all_counties = read.County.to_list()
 
 answer = screen.textinput(title='Ireland Map', prompt='Name a county!')
+score = 0
 
 while answer != 'done':
     if answer in all_counties:
+        score += 1
+        screen.title('Ireland Map - Score: ' + str(score) + '/33')
         t = turtle.Turtle()
         t.penup()
         t.hideturtle()
         county_selected = read[read.County == answer]
         t.goto(int(county_selected.x), int(county_selected.y))
         t.write(answer, font=('Arial', 10, 'normal'))
+        if score == 33:
+            print('You win!')
+            break
 
     else:
         print('incorrect')
