5684b0bd32a4c96c670e3d1a9ac7cc59a2db5e93
Craig Sheehy <craigsheehy123@gmail.com>
2023-06-21 12:51:40 +0100

    - Introduction into TKinter

---
+++ Intro TKinter/main.py
@@ -0,0 +1,18 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("Custom Title")
+window.minsize(width=600, height=600)
+
+# Components
+
+# Labels
+label = tkinter.Label(text='I am a label', font=("Arial", 14, "bold"))
+label.pack()
+
+
+
+
+
+window.mainloop()
+# Do not put code below here
---
+++ Intro TKinter/playground.py
@@ -0,0 +1,26 @@
+def add(*args):
+    sum = 0
+    for n in args:
+        sum += n
+    return sum
+
+
+print(add(5, 6, 7, 8))
+
+
+def calculate(**kwargs):
+    print(kwargs)
+
+
+calculate(name='Craig', surname='Sheehy')
+
+class Car:
+
+    def __init__(self, **kwargs):
+        self.make = kwargs.get('make')
+        self.model = kwargs.get('model')
+        self.colour = kwargs.get('colour')
+
+
+my_car = Car(make='Audi', model='R8')
+print(my_car.colour)
