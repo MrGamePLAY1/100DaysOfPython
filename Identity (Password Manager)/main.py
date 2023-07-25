319cfb50781a2ed2e7a0e885309604494e3bffdd
Craig Sheehy <craigsheehy123@gmail.com>
2023-07-25 10:52:31 +0100

    - added search to passwords

--- Identity (Password Manager)/main.py
+++ Identity (Password Manager)/main.py
@@ -25,7 +25,7 @@ def save_password():
     password_contents = password_entry.get()
 
     while not_saved:
-        if website_entry.get() == "" or email_entry.get() == "" or password_entry.get() == "":
+        if website_contents == '' or email_contents == '' or password_contents == '':
             validations()
             break
         else:
@@ -37,36 +37,32 @@ def save_password():
                 }
             }
 
-            # try, except
+            # try: Read the password
             try:
                 with open('passwords.json', 'r') as data_file:
                     # Read Data
                     data = json.load(data_file)
-
+            # if no file is found, create one
             except FileNotFoundError:
                 print('Sorry no file found, creating one now')
                 with open('passwords.json', 'w') as data_file:
                     json.dump(new_data, data_file, indent=4)
-            else:
-                data.update(new_data)
-                with open('passwords.json', 'w') as data_file:
-                    json.dump(new_data, data_file, indent=4)
-                    success_label = Label(window, text="Success!", fg="green")
-                    success_label.grid(row=4, column=0)
-                    window.after(2000, remove_label, success_label)
-                    not_saved = False
+            data.update(new_data)
+            # if everything is okay do the following
+            with open('passwords.json', 'w') as data_file:
+                json.dump(data, data_file, indent=4)
 
-            finally:
-                # Clearing all the feeds
-                website_entry.delete(0, END)
-                email_entry.delete(0, END)
-                password_entry.delete(0, END)
+                success_label = Label(window, text="Success!", fg="green")
+                success_label.grid(row=4, column=0)
+                window.after(2000, remove_label, success_label)
+                not_saved = False
 
-
-
+            # Clearing all the feeds
+            website_entry.delete(0, END)
+            email_entry.delete(0, END)
+            password_entry.delete(0, END)
 
     website_entry.focus()
-
 
 
 def validations():
@@ -83,6 +79,29 @@ def read_for_passwords():
         data_file = json.load(data_file)
         return data_file
         # print(data_file)
+
+
+def search_passwords():
+    # Get the elements in the website
+    website_contents = website_entry.get()
+    website_contents = website_contents.lower()
+
+    # Once we read the website entry use it as a key to search the data
+    with open('passwords.json', 'r') as data_file:
+        data = json.load(data_file)
+
+        # Iterate through the keys in the data dictionary
+        for website_name, website_data in data.items():
+            if website_contents == website_name.lower():
+                print('Data', website_data)
+                messagebox.showinfo("Message", website_data['email']['password'])
+
+            # print(f"Website Name: {website_name}")
+            # print("Website Data:")
+            # print(website_data)
+            # print()
+
+
 
 
 # ------------------- UI ----------------------------------- #
@@ -103,14 +122,15 @@ canvas.grid(row=0, column=0, columnspan=3)
 website = Label(window, text="Website:")
 email = Label(window, text="Email:")
 password = Label(window, text="Password:")
+search = Label(window, text="Search")
 
 website.grid(row=1, column=0)
 email.grid(row=2, column=0)
 password.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(window, width=43)
-website_entry.grid(row=1, column=1, columnspan=2, pady=5)
+website_entry = Entry(window, width=22)
+website_entry.grid(row=1, column=1, columnspan=1, pady=5)
 website_entry.focus()
 
 email_entry = Entry(window, width=43)
@@ -125,5 +145,8 @@ generate_pass.grid(row=3, column=2, pady=5, padx=12, sticky=W)
 
 add_button = Button(text="Add", width=36, command=save_password)
 add_button.grid(row=4, column=1, columnspan=2, pady=5)
+
+search_button = Button(text='Search', width=15, command=search_passwords)
+search_button.grid(row=1, column=2, pady=5, padx=12, sticky=W)
 
 window.mainloop()
