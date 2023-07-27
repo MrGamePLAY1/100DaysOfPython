b2bf811feb8a633261780f62602e5b696faf52f2
Craig Sheehy <craigsheehy123@gmail.com>
2023-07-27 15:56:55 +0100

    - Formatted code

--- Flash Car App/main.py
+++ Flash Car App/main.py
@@ -15,15 +15,15 @@ canvas.create_text(400, 150, text='Title', font=('Ariel', 40, 'italic'))
 canvas.create_text(400, 263, text='Word', font=('Ariel', 60, 'bold'))
 
 canvas.config(background=BACKGROUND_COLOR, highlightthickness=0)
-canvas.grid(row=0, column=0)
+canvas.grid(row=0, column=0, columnspan=2)
 
 cross_image = PhotoImage(file='images/wrong.png')
 unknown_button = Button(image=cross_image)
-unknown_button.grid(row=1, column=0, sticky=W)
+unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file='images/right.png')
 correct_button = Button(image=check_image)
-correct_button.grid(row=1, column=1, columnspan=1)
+correct_button.grid(row=1, column=1)
 
 
 
