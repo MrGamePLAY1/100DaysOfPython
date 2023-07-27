41184c2646453720b241a7bbe101f2641829e634
Craig Sheehy <craigsheehy123@gmail.com>
2023-07-27 13:18:13 +0100

    - Updated button positions

--- Flash Car App/main.py
+++ Flash Car App/main.py
@@ -19,10 +19,12 @@ canvas.grid(row=0, column=0)
 
 cross_image = PhotoImage(file='images/wrong.png')
 unknown_button = Button(image=cross_image)
-unknown_button.grid(row=1, column=0)
+unknown_button.grid(row=1, column=0, sticky=W)
 
 check_image = PhotoImage(file='images/right.png')
 correct_button = Button(image=check_image)
-correct_button.grid(row=1, column=1)
+correct_button.grid(row=1, column=1, columnspan=1)
+
+
 
 window.mainloop()
