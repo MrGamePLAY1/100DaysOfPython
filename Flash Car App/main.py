4d9b59a55529d04e2e3c8697d4b15958695d23ee
Craig Sheehy <craigsheehy123@gmail.com>
2023-07-27 16:27:50 +0100

    - Added random words to display

--- Flash Car App/main.py
+++ Flash Car App/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from functions import *
 
 BACKGROUND_COLOR = "#B1DDC6"
 
@@ -6,25 +7,24 @@ window = Tk()
 window.title('Flash')
 window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
 
+
 canvas = Canvas(width=800, height=526)
 card_front_image = PhotoImage(file='images/card_front.png')
 canvas.create_image(400, 263, image=card_front_image)
 
 # Text
-canvas.create_text(400, 150, text='Title', font=('Ariel', 40, 'italic'))
-canvas.create_text(400, 263, text='Word', font=('Ariel', 60, 'bold'))
+canvas.create_text(400, 150, text='Title', font=('Ariel', 30, 'italic'))
+word = canvas.create_text(400, 263, text='Word', font=('Ariel', 60, 'bold'))
 
 canvas.config(background=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
 cross_image = PhotoImage(file='images/wrong.png')
-unknown_button = Button(image=cross_image)
+unknown_button = Button(image=cross_image, highlightthickness=0, command=random_word)
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file='images/right.png')
-correct_button = Button(image=check_image)
+correct_button = Button(image=check_image, highlightthickness=0, command=random_word)
 correct_button.grid(row=1, column=1)
-
-
 
 window.mainloop()
