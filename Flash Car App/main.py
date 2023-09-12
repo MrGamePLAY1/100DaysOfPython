71dc70ddb829319fa655da79f1362f0be3e0de48
Craig Sheehy <craigsheehy123@gmail.com>
2023-09-12 11:19:30 +0100

    - Improved rnadomm word function

--- Flash Car App/main.py
+++ Flash Car App/main.py
@@ -2,27 +2,39 @@ from tkinter import *
 import pandas as pd
 import random
 
+current_word = {}
+data = pd.read_csv('data/french_words.csv')
+to_learn = data.to_dict(orient='records')
+
+
+def random_word():
+    global current_word
+    french = data['French'].to_list()
+    current_word = random.choice(to_learn)
+    canvas.itemconfig(card_title, text='French')
+    canvas.itemconfig(card_word, text=current_word['French'])
+
+
+def french_to_english():
+    canvas.itemcget(card_title, text='English')
+    canvas.itemcget(card_word, text=current_word['English'])
+
+# french_to_english()
+
 BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
 window.title('Flash')
 window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
-
-def random_word():
-    dataframe = pd.read_csv('data/french_words.csv')
-    french = dataframe['French'].to_list()
-    random_french = random.choice(french)
-    canvas.itemconfig(card_title, text='French')
-    canvas.itemconfig(card_word, text=random_french)
-
+window.after(3000, func=french_to_english)
 
 canvas = Canvas(width=800, height=526)
 card_front_image = PhotoImage(file='images/card_front.png')
 canvas.create_image(400, 263, image=card_front_image)
 
 # Text
-card_title = canvas.create_text(400, 150, text='Title', font=('Ariel', 30, 'italic'))
-card_word = canvas.create_text(400, 263, text='Word', font=('Ariel', 60, 'bold'))
+card_title = canvas.create_text(400, 150, text='', font=('Ariel', 30, 'italic'))
+card_word = canvas.create_text(400, 263, text='', font=('Ariel', 60, 'bold'))
 
 canvas.config(background=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
@@ -35,4 +47,7 @@ check_image = PhotoImage(file='images/right.png')
 correct_button = Button(image=check_image, highlightthickness=0, command=random_word)
 correct_button.grid(row=1, column=1)
 
+
+# Start displaying the new French word
+random_word()
 window.mainloop()
